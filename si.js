e77530340850f017b026130bbeddab98b1221040
pratham-9029 <saixerox5075@gmail.com>
2025-08-27 21:53:24 +0530

    Create si.js

---
+++ si.js
@@ -0,0 +1,23 @@
+   let P, R, T;
+
+        P = 300000;
+        T = 10;
+
+        if (P >= 500000 && T >= 10) {
+            R = 9.5;
+        }
+        else if (P >= 300000 && T >= 8) {
+            R = 7.5;
+        }
+        else if (P >= 150000 && T >= 5) {
+            R = 4.5;
+        }
+        else {
+            R = 2.5;
+        }
+
+        let SI = P * R * T / 100;
+
+        let netPrice = P + SI ;
+
+        console.log(netPrice);
